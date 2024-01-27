67ba1193d73988f27dd15973ae09075278c5e16c
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-01-27 18:43:57 +0400

    fix vs folder

--- Services/Services.Coupon.API/obj/Debug/net8.0/Services.Coupon.API.AssemblyInfo.cs
+++ Services/Services.Coupon.API/obj/Debug/net8.0/Services.Coupon.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services.Coupon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65ecc37abd89da0f0f8b0b91dd80d7c522384ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12dfc2fc2f95fdc361d4ee141e6ebd12265320e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services.Coupon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services.Coupon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

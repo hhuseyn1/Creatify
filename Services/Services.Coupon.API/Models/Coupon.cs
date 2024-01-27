3dc3b675b662f3aa656abf2a37eb489195dbdb16
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-01-27 13:42:59 +0400

    add coupon model

---
+++ Services/Services.Coupon.API/Models/Coupon.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.Coupon.API.Models;
+
+public class Coupon
+{
+    public int Id { get; set; }
+    public string CouponCode { get; set; }
+    public double DiscountAmount{ get; set; }
+    public int MinAmount { get; set; }
+}

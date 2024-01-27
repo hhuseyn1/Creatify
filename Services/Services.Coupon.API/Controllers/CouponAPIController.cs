03bc7c3f4291bc73c2e9d3a0714c981214a52e11
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-01-27 22:41:31 +0400

    Common response added

--- Services/Services.Coupon.API/Controllers/CouponAPIController.cs
+++ Services/Services.Coupon.API/Controllers/CouponAPIController.cs
@@ -1,49 +1,54 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Services.Coupon.API.Data;
+using Services.Coupon.API.Models.Dto;
 
 namespace Services.Coupon.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CouponController : ControllerBase
+public class CouponAPIController : ControllerBase
 {
     private readonly AppDbContext _context;
+    private ResponseDto _response;
 
-    public CouponController(AppDbContext context)
+    public CouponAPIController(AppDbContext context)
     {
         _context = context;
+        _response = new ResponseDto();
     }
 
     [HttpGet]
-    public object GetCoupon()
+    public ResponseDto GetCoupon()
     {
         try
         {
             IEnumerable<Models.Coupon> objList = _context.Coupons.ToList();
-            return objList;
+            _response.Result = objList;
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex.Message);
+            _response.isSuccess = false;
+            _response.Message = ex.Message;
         }
-        return null;
+        return _response;
     }
 
     [HttpGet]
     [Route("{id:int}")]
-    public object GetCouponbyId(int id)
+    public ResponseDto GetCouponbyId(int id)
     {
         try
         {
             Models.Coupon obj = _context.Coupons.First(u=>u.Id == id);
-            return obj;
+            _response.Result = obj;
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex.Message);
+            _response.isSuccess = false;
+            _response.Message = ex.Message;
         }
-        return null;
+        return _response;
     }
 
 }

74fe4836e1bd0269c347c696d491408712e1fbe1
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-01-27 22:25:24 +0400

    Get methods added

---
+++ Services/Services.Coupon.API/Controllers/CouponController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Coupon.API.Data;
+
+namespace Services.Coupon.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CouponController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public CouponController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public object GetCoupon()
+    {
+        try
+        {
+            IEnumerable<Models.Coupon> objList = _context.Coupons.ToList();
+            return objList;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return null;
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public object GetCouponbyId(int id)
+    {
+        try
+        {
+            Models.Coupon obj = _context.Coupons.First(u=>u.Id == id);
+            return obj;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return null;
+    }
+
+}
--- Services/Services.Coupon.API/obj/Debug/net8.0/Services.Coupon.API.AssemblyInfo.cs
+++ Services/Services.Coupon.API/obj/Debug/net8.0/Services.Coupon.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services.Coupon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a0476e7627c109254655c69f29442e35eeb2883")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac5830a068947af9eccf4ee8b1a842bd1b1a636")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services.Coupon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services.Coupon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
